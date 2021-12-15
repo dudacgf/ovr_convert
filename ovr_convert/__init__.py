894d0d3d14391a75a6000280d5eb8ae589390848
Eduardo Ferreira <@dudacgf>
2021-12-14 23:20:43 -0300

    refactor to new run.py launch script. remove unused imports

--- ovr_convert/__init__.py
+++ ovr_convert/__init__.py
@@ -1,84 +1,75 @@
 import os
 
-from flask import Flask, render_template, session
+from flask import Flask, render_template
 from flask_session import Session
 from flask_bootstrap import Bootstrap
-from flask_wtf import CSRFProtect
 
-from .gvm_api import gvm_bp
-from .api import api_bp
 from .api import new_session_configuration
 from .gvm_api import get_gvm_sock, login_gvm, get_tasks
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(
-      SECRET_KEY = os.urandom(24),
-      UPLOAD_FOLDER = '/var/tmp',
-      MAX_CONTENT_LENGTH = 200*1024*1024, # to generate very big reports. ymmv 
-      SESSION_PERMANENT = False,
-      SESSION_TYPE = 'filesystem',
-      SESSION_COOKIE_SAMESITE = "Lax",
-      BOOTSTRAP_BOOTSWATCH_THEME = 'cosmo',
-      BOOTSTRAP_BTN_SIZE = 'sm',
-      GVM_SOCK_PATH = '/run/sock/gvm.sock',
-    )
+app = Flask(__name__)
+app.config.from_mapping(
+  SECRET_KEY = os.urandom(24),
+  UPLOAD_FOLDER = '/var/tmp',
+  MAX_CONTENT_LENGTH = 200*1024*1024, # to generate very big reports. ymmv 
+  SESSION_PERMANENT = False,
+  SESSION_TYPE = 'filesystem',
+  SESSION_COOKIE_SAMESITE = "Lax",
+  BOOTSTRAP_BOOTSWATCH_THEME = 'cosmo',
+  BOOTSTRAP_BTN_SIZE = 'sm',
+  GVM_SOCK_PATH = '/run/sock/gvm.sock',
+)
 
-    # create csfr token protection
-    csrf = CSRFProtect(app)
+# create server-side session
+Session(app)
+
+# register general api
+try:
+    app.register_blueprint(api.api_bp)
+except:
+    raise Exception('could not register api blueprint.')
+
+# register gvm api
+try:
+    app.register_blueprint(gvm_api.gvm_bp)
+except:
+    raise Exception('could not register gvm blueprint.')
+
+# create Bootstrap
+bootstrap = Bootstrap(app)
+
+# create instance dirs if any
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
     
-    # create server-side session
-    Session(app)
+# try to connect to gvm via it's socket
+if get_gvm_sock():
+    app.add_template_global(name='GVM_SOCK_PRESENT', f=True)
+else:
+    app.add_template_global(name='GVM_SOCK_PRESENT', f=False)
     
-    # register general api
-    try:
-        app.register_blueprint(api.api_bp)
-    except:
-        raise Exception('could not register api blueprint.')
+# define filter to jinja2 later use
+@app.template_filter('lowerCamelCase')
+def lowerCamelCase(s):
+    words = s.translate(str.maketrans({'_': ' ', '-': ' ', '#': ' '})).split(' ')
+    s = "".join(word[0].upper() + word[1:].lower() for word in words)
+    return s[0].lower() + s[1:]
 
-    # register gvm api
-    try:
-        app.register_blueprint(gvm_api.gvm_bp)
-    except:
-        raise Exception('could not register gvm blueprint.')
-    
-    # create Bootstrap
-    bootstrap = Bootstrap(app)
+@app.route('/')
+def btf():
+    new_session_configuration()
 
-    # create instance dirs if any
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-        
-    # try to connect to gvm via it's socket
-    if get_gvm_sock():
-        app.add_template_global(name='GVM_SOCK_PRESENT', f=True)
-    else:
-        app.add_template_global(name='GVM_SOCK_PRESENT', f=False)
-        
-    # define filter to jinja2 later use
-    @app.template_filter('lowerCamelCase')
-    def lowerCamelCase(s):
-        words = s.translate(str.maketrans({'_': ' ', '-': ' ', '#': ' '})).split(' ')
-        s = "".join(word[0].upper() + word[1:].lower() for word in words)
-        return s[0].lower() + s[1:]
-    
-    @app.route('/')
-    def btf():
-        new_session_configuration()
+    return render_template('index.html')
 
-        return render_template('index.html')
-
-    @app.route('/test')
-    def test():
-        if app.jinja_env.globals['GVM_SOCK_PRESENT']:
-            if login_gvm():
-                return get_tasks()
-            else:
-                return 'could not login'
+@app.route('/test')
+def test():
+    if app.jinja_env.globals['GVM_SOCK_PRESENT']:
+        if login_gvm():
+            return render_template('test.html', tasks=get_tasks())
         else:
-            return 'no gvm sock to be used'
-       
-    return app
+            return 'could not login'
+    else:
+        return 'no gvm sock to be used'
 
